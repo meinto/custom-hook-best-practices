9cddb342c85b5317764ba7dbe537ff62ab874b77
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2021-02-07 20:56:35 +0100

    Comment

--- src/non-global-references/stateObjectReference.ts
+++ src/non-global-references/stateObjectReference.ts
@@ -8,6 +8,11 @@ const sample = {
 
 export const useFail = () => ({ ...sample })
 
+/**
+ * If you have mutate the object this solition is not optimal,
+ *  because it triggers an additional rerender.
+ * Use useRef instead (see refObjectReference.ts)
+ */
 export const useSolution = () => {
   const [stateObject] = useState({ ...sample })
   return stateObject
